8c336ef36a96c3fca1680b3a026e34722d4be039
NalukurthiBhavana07 <nalukurthibhavana.24.ece@anits.edu.in>
2026-03-12 21:54:58 +0530

    Create Student-Id.py

---
+++ Student-Id.py
@@ -0,0 +1,93 @@
+ class Student:
+
+def __init__ (Self, Sid, name, mi, ma, m3, dob, fee-paid):
+
+Self.Sid=Sid
+
+Self.name=name
+
+Self.marks=[m1,,m2,m3]
+
+self.dob = dob
+
+Self.fee-paid=fee-paid
+
+Self.total fee = 50000
+
+def calculate_Cgpa (self):
+
+avg=Sum (Self.marks)/3
+
+return (avg/10)
+
+def calculate_age (Self):
+
+age = 2026_Self. dob
+
+return (age)
+
+def Calculate_fee_balance (self):
+
+balance self. total_fee_self. fee_paid
+
+def prind-Sd (self):
+
+print ("Student ID: ", self. sid)
+
+print ("Name: ", self. name)
+
+print ("CGPA:" Self. calculate_cgpa())
+
+Print ("Age:", self. calculate_age())
+
+print ("Fee Balance:",self. calculate_fee_balance ())
+
+class College:
+
+def __init__ (self, Code, name, Location, 51, 53, 53):
+
+Self.college_code=code
+
+self. college_name=name
+
+Self. location=location
+
+Self.s1=s1
+
+self. s2=s2
+
+self.s3=s3
+
+def display_report (self):
+
+print ("COLLEGE DETAILS")
+
+print ("Name:", self.college_name)
+
+print ("Code:", self.college_code)
+
+print ("Location:", self. location)
+
+print ("STUDENT ACADEMIC DETAILS")
+
+Self.s1 print_sd()
+
+Seif.s2 print_sd()
+
+Self.s3 print_sd()
+
+#creating objects for student class
+
+Std1=Student (15, "Karthika", 90, 91, 85, 2006, 50000)
+
+Std2=student (25, "Jashmitha", $55, 89, 95, 2006, 45000)
+
+Std3=Student (42, "Bhavana", 88, 90, 89, 2006, 46000)
+
+#creating object for college class
+
+clg: college ("ANESOOI", "ANITS", "Visakhapatnam", Stdi, Stdz, Sta3)
+
+# display report
+
+clg. display-report()
