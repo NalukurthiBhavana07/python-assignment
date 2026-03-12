17f9cd31c4685d0d952c591ce7ddcc95ca77b6a6
NalukurthiBhavana07 <nalukurthibhavana.24.ece@anits.edu.in>
2026-03-12 22:02:56 +0530

    Create Student-Id.ouput.py

---
+++ Student-Id.ouput.py
@@ -0,0 +1,39 @@
+COLLEGE DETAILS
+
+Name: ANITS
+
+Coda: ANES001
+
+Location: Visakhapatnam
+
+STUDENT ACADEMIC DETAILTS
+
+Student ID: 15
+
+Name: Karthika.
+
+CGPA: 8.8666
+
+Age: 20
+
+Fee Balance:o
+
+Student ID: 25
+
+Name: Jashmitha
+
+CGPA: 8.9666
+
+Age: 20
+
+Fee Balance: 5000
+
+Student ID: 42
+
+Name: Bhavana
+
+CGPA: 8.9
+
+Age: 20
+
+Fee Balance: 4000
